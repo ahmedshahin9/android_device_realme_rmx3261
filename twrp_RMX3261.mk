e1c2737e98c614d05ec383abe947431da682b18a
Ahmed Shahin <83367407+ahmedshahin9@users.noreply.github.com>
2024-06-30 02:30:06 +0300

    Update twrp_RMX3261.mk

--- twrp_RMX3261.mk
+++ twrp_RMX3261.mk
@@ -7,23 +7,20 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from RMX3261 device
 $(call inherit-product, device/realme/RMX3261/device.mk)
 
+# Device identifier
 PRODUCT_DEVICE := RMX3261
 PRODUCT_NAME := twrp_RMX3261
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3261
 PRODUCT_MANUFACTURER := realme
-
-PRODUCT_GMS_CLIENTID_BASE := android-realme
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="S19610AA1_ctcc-user 11 RP1A.201005.001 06322 release-keys"
-
-BUILD_FINGERPRINT := realme/RMX3261/RMX3261:11/RP1A.201005.001/1675866988000:user/release-keys
